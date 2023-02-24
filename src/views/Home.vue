24fa3da69111b678e3549a6acf6b3ab308aaf72e
Robert Regalado <rjregalado1979@gmail.com>
2023-02-24 21:34:30 +0800

    add watch  and watch effec

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="home">
-    <p>Homepage</p>
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="handleClick">stop watching</button>
   </div>
 </template>
 
 <script>
-
+import { computed, reactive, ref, watch, watchEffect } from 'vue'
 export default {
-  name: 'Home'
-
+  name: 'Home',
+  setup() {    
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
+    const matchingNames = computed(() => {
+      return names.value.filter(name => name.includes(search.value))
+    })
+    const stopWatch = watch(search, () => {
+      console.log('watch function ran')
+    })
+    const stopEffect = watchEffect(() => {
+      console.log('watchEffect ran', search.value)
+      console.log(names.value)
+    })
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+    return { names, search, matchingNames, handleClick }
+  },
 }
 </script>
